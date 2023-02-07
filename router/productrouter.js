43f076458a770b7d4b40a38205585848dadaf319
jeevan aloor <jeevanaloor500@gmail.com>
2023-02-07 19:43:31 +0530

    add deploye

---
+++ router/productrouter.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const {ClothModel}=require("../conflict/model")
+
+
+const productRouter = express.Router();
+
+
+productRouter.get("/getcloths",async(req,res)=>{
+    let query=req.query.clothcategory
+    let rate=req.query.clothrate
+    console.log("rate",rate)
+  
+
+    try {
+        if(query==undefined){
+            let data=await ClothModel.find().sort({ rate: "asc" })
+            res.send(data)
+            console.log(data) 
+
+        }else{
+            let data=await ClothModel.find({clothcategory:query})
+            res.send(data)
+            console.log(data)
+
+        }
+        
+         
+        
+    } catch (error) {
+        console.log(error)
+        console.log("error")
+        
+    }
+    
+})
+
+productRouter.post("/postdata",async(req,res)=>{
+    const {clothimage,clothname,clothrate,clothstrikerate,clothcategory}=req.body
+    try {
+        const user=new ClothModel({
+            clothimage,
+            clothname,
+            clothrate,
+            clothstrikerate,
+            clothcategory
+
+        })
+        await user.save()
+        res.send("done")
+        console.log("done")
+        
+    } catch (error) {
+        res.send("error in posting")
+        console.log("error in posting")
+        console.log(error)
+        
+    }
+
+})
+
+module.exports={
+    productRouter
+}
